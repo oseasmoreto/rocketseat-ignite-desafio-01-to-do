ae214e4043b2e7e2b0d0e2ce03391181cdc4cebf
oseasmoreto <oseas_moreto@hotmail.com>
2022-07-20 15:43:01 -0300

    Ini Project Vite + ReactJS + TS

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import styles from './index.module.css'
+
+function App() {
+  return (
+    <div className="App">
+      <p>TO-do</p>
+    </div>
+  )
+}
+
+export default App
