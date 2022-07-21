ab982528b7f4937517a947af5c546ca1415b43fc
oseasmoreto <oseas_moreto@hotmail.com>
2022-07-21 01:05:26 -0300

    Adiciona dados em array aos componentes

--- src/components/TaskContainer/index.tsx
+++ src/components/TaskContainer/index.tsx
@@ -6,34 +6,60 @@ import styles from './index.module.css';
 
 export function TaskContainer() {
 
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      completed: false,
+      content: "Fazer curso do mês."
+    }
+  ]);
+  const [completedTasks, setCompletedTasks] = useState([
+    {
+      id: 1,
+      completed: true,
+      content: "Ir na academia."
+    },
+    {
+      id: 2,
+      completed: true,
+      content: "Comer um pastel"
+    }]);
 
   return (
     <div className={styles.tasksContainer}>
       <div className={styles.info}>
-        <p className={styles.tasksCreated}>Tarefas criadas <span className={styles.counterTasks}>0</span></p>
-        <p className={styles.tasksCompleted}>Concluídas <span className={styles.counterTasks}>0</span></p>
+        <p className={styles.tasksCreated}>Tarefas criadas <span className={styles.counterTasks}>{ tasks.length }</span></p>
+        <p className={styles.tasksCompleted}>Concluídas <span className={styles.counterTasks}>{ completedTasks.length } de {completedTasks.length + tasks.length}</span></p>
       </div>
 
-      <NoTask />
+      {tasks.length == 0 && <NoTask /> }
       
-      <div className={styles.boxTasks}>
-          <div className={styles.task}>
-            <input type="checkbox" />
-            <p>
-              Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer. 
-              Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-            </p>
-            <button className={styles.deleteTask}><Trash size="1.5rem" /></button>
-          </div>
-          <div className={styles.task}>
-            <input type="checkbox" checked />
-            <p className={ styles.taskCompleted }>
-              Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-            </p>
-            <button className={styles.deleteTask}><Trash size="1.5rem" /></button>
-          </div>
-      </div> 
+      {tasks.length > 0 &&
+        <div className={styles.boxTasks}>
+          { tasks.map(task =>{
+            return (
+              <div key={task.id} className={styles.task}>
+                <input type="checkbox" />
+                <p>
+                  { task.content}
+                </p>
+                <button className={styles.deleteTask}><Trash size="1.5rem" /></button>
+              </div>
+            )
+          }) }
+          { completedTasks.map(task =>{
+            return (
+              <div key={task.id} className={styles.task}>
+                <input checked type="checkbox" />
+                <p className={ styles.taskCompleted }>
+                  { task.content}
+                </p>
+                <button className={styles.deleteTask}><Trash size="1.5rem" /></button>
+              </div>
+            )
+          }) }
+        </div> 
+      }
     </div>
   )
 }
